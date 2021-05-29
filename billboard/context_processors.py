daa7812a50549d02e04da69124df1477fc567117
rippedpleasure <maeror577@gmail.com>
2021-05-29 15:13:30 +0500

    Fix context_processors when user is not authenticated

--- billboard/context_processors.py
+++ billboard/context_processors.py
@@ -2,6 +2,8 @@ from ads.models import Response
 
 
 def counters(request):
-    return {
-        'user_responses': Response.objects.filter(ad__author=request.user)
-    }
+    if request.user.is_authenticated:
+        return {
+            'user_responses': Response.objects.filter(ad__author=request.user)
+        }
+    return request
